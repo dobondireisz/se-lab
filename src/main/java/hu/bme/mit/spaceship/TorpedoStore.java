07bb13abe7010e9214901ffad260cf0b83dd6eed
Dobondi-Reisz Hanna <hannadobondireisz22@gmail.com>
2024-10-15 23:57:17 +0200

    throw

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -29,10 +29,10 @@ public class TorpedoStore {
   }
 
   Random generator = new Random();
-  
+
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
